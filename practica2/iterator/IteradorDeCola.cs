3664fff9e4a75deab38198b31a3fc0fa9d57e0b5
osirisdavid <davidosirisayala@gmail.com>
2024-05-12 20:11:13 -0300

    se agrega iterador de cola

---
+++ practica2/iterator/IteradorDeCola.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: osiri
+ * Fecha: 12/5/2024
+ * Hora: 20:08
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Practicas.practica2.iIterator
+{
+	/// <summary>
+	/// Description of InteradorDeCola.
+	/// </summary>
+	public class InteradorDeCola
+	{
+		public InteradorDeCola()
+		{
+		}
+	}
+}
